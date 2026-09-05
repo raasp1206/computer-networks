7ce417a24bf2df450165a67339a91b0fd91b5d7a
Sathya Priya <sathyapriya281206_bcs28@mepcoeng.ac.in>
2026-09-05 12:12:23 +0530

    Create SlidingWindowMax.c

---
+++ EX5/SlidingWindowMax.c
@@ -0,0 +1,23 @@
+from collections import deque
+
+class Solution(object):
+    def maxSlidingWindow(self, nums, k):
+        if not nums or k == 0:
+            return []
+        
+        result = []
+        window = deque()
+        
+        for i, num in enumerate(nums):
+            if window and window[0] < i - k + 1:
+                window.popleft()
+                
+            while window and nums[window[-1]] < num:
+                window.pop()
+                
+            window.append(i)
+            
+            if i >= k - 1:
+                result.append(nums[window[0]])
+                
+        return result
